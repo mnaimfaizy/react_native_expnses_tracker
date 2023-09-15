1b2b232c4d3f609815c1f4a4c2ebf90a63a4a022
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-09-15 18:05:50 +0500

    chore: implemented the add, edit and delete for expenses using context API

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,11 +1,14 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
@@ -15,11 +18,31 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
 
-  function cancelHandler() {}
+  function cancelHandler() {
+    navigation.goBack();
+  }
 
-  function confirmHandler() {}
+  function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: "Text",
+        amount: 199.99,
+        date: new Date("2022-05-19"),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: "Text",
+        amount: 199.99,
+        date: new Date("2022-05-19"),
+      });
+    }
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
